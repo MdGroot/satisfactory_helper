5f2c24a03ee286c4d49febfdfc1f85cbdee461f1
Maarten de Groot <m.l.d.groot@student.tue.nl>
2023-02-23 21:04:33 +0100

    some formatting and adding loop in main

--- helper.c
+++ helper.c
@@ -19,8 +19,7 @@ void makeMachinefile(){
         fclose(fp);
         return;
     }
-    
-    printf("Error: can't open file to read\n");
+    printf("can't open file to read\n");
     fp = fopen("machines.dat", "wb");
     int begin = 0;
     fwrite(&begin, sizeof(&begin), 1, fp);
@@ -41,7 +40,7 @@ void readMachines(){
     printf("maximum = %d\n", maximum);
     Machine *machines = malloc(maximum * sizeof(&machines));
     for(int i = 0; i <= maximum; ++i){
-        fread(&machines[i], sizeof(&machines[i]), 1, fp); 
+        fread(&machines[i], sizeof(machines[i]), 1, fp); 
         printf("machine name: %s\tmaterial output: %s, %d\tmaterial intput: %s, %d %s, %d\n", 
                 machines[i].name, machines[i].out.name, machines[i].out.amount, machines[i].in1.name, 
                 machines[i].in1.amount, machines[i].in2.name, machines[i].in2.amount);
@@ -51,51 +50,50 @@ void readMachines(){
     return;
 };
 
-void addmachines(){
+void addMachines(){
     // TODO : make funciton to add machine to machines array and file
 };
 
-void removemachines(){
+void removeMachines(){
     // TODO : make function to remove machine from machines array and file
 };
 
-void makecalculation(){
+void makeCalculation(){
     // TODO : make function to calculate necessary materials based on end result
 };
 
-int main(int arc, char* argv[])
+int main(int arc, char** argv)
 {
     makeMachinefile();
     readMachines();
     int choice;
-
-    printf("1. Read Machines\n");
-    printf("2. Add Machines\n");
-    printf("3. Remove Machines\n");
-    printf("4. Make Calculation\n");
-    printf("Enter your choice: ");
-
-    // Read the user's choice
-    scanf("%1d", &choice);
-
-    // Switch statement to handle the user's choice
-    switch (choice)
-    {
-        case 1:
-            readMachines();
-            break;
-        case 2:
-            addmachines();
-            break;
-        case 3:
-            removemachines();
-            break;
-        case 4:
-            makecalculation();
-            break;
-        default:
-            printf("menu error\n");
-            break;
+    while(1){
+        printf("1. Read Machines\n");
+        printf("2. Add Machines\n");
+        printf("3. Remove Machines\n");
+        printf("4. Make Calculation\n");
+        printf("Enter your choice: ");
+    
+        scanf("%1d", &choice);
+    
+        switch (choice)
+        {
+            case 1:
+                readMachines();
+                break;
+            case 2:
+                addMachines();
+                break;
+            case 3:
+                removeMachines();
+                break;
+            case 4:
+                makeCalculation();
+                break;
+            default:
+                printf("menu error\n");
+                break;
+        }
     }
     return 0;
 }
